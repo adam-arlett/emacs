d8602e00202be452036d0f851456e1b56c2b9f02
Adam Arlett <42839753+adam-arlett@users.noreply.github.com>
2020-05-18 20:55:21 +0200

    Add files via upload
    Shamelessly cribbed emacs config

---
+++ init.el
@@ -0,0 +1,43 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+'("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+(package-refresh-contents)
+(package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/innit.org"))
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'paredit-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(doom-modeline-minor-modes t)
+ '(org-confirm-babel-evaluate nil)
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Sync/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-src-fontify-natively t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
+ '(package-selected-packages
+   (quote
+    (clj-refactor cider htmlize ox-reveal noflet org-bullets doom-modeline auto-complete smex expand-region beacon yasnippet git-timemachine git-gutter exec-path-from-shell treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs rainbow-delimiters paredit flycheck ace-window which-key use-package try posframe all-the-icons-dired))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
